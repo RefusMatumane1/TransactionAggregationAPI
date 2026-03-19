ad55e840ff50a16f87bbd2859eddc5d120acbf0f
princematumane <princematumane@gmail.com>
2026-03-19 23:49:22 +0200

    Add scafolding of redis and postgres

--- TransactionAggregationAPI/TransactionAggregationAPI.AppHost/AppHost.cs
+++ TransactionAggregationAPI/TransactionAggregationAPI.AppHost/AppHost.cs
@@ -1,5 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.TransactionAggregationAPI>("transactionaggregationapi");
+var posgress = builder.AddPostgres("transaction-db")
+    .WithDataVolume()
+    .WithPgAdmin();
+
+var redis = builder.AddRedis("transaction-cache").WithDataVolume();
+
+builder.AddProject<Projects.TransactionAggregationAPI>("transactionaggregationapi")
+    .WithHttpsEndpoint(5001, name:"public")
+    .WithReference(posgress)
+    .WaitForStart(posgress)
+    .WithReference(redis)
+    .WaitForStart(redis);
 
 builder.Build().Run();
